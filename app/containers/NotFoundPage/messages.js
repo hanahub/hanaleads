fbd3b38501091aab3980f5327004b70346d0e92a
Valentin Marinov <dev.valentin2013@gmail.com>
2018-08-13 17:08:45 -0400

    update

--- app/containers/NotFoundPage/messages.js
+++ app/containers/NotFoundPage/messages.js
@@ -1,13 +1,30 @@
 /*
- * NotFoundPage Messages
+ * HomePage Messages
  *
- * This contains all the text for the NotFoundPage component.
+ * This contains all the text for the HomePage component.
  */
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  header: {
-    id: 'boilerplate.containers.NotFoundPage.header',
-    defaultMessage: 'Page not found.',
+  startProjectHeader: {
+    id: 'boilerplate.containers.HomePage.start_project.header',
+    defaultMessage: 'Start your next react project in seconds',
+  },
+  startProjectMessage: {
+    id: 'boilerplate.containers.HomePage.start_project.message',
+    defaultMessage:
+      'A highly scalable, offline-first foundation with the best DX and a focus on performance and best practices',
+  },
+  trymeHeader: {
+    id: 'boilerplate.containers.HomePage.tryme.header',
+    defaultMessage: 'Try me!',
+  },
+  trymeMessage: {
+    id: 'boilerplate.containers.HomePage.tryme.message',
+    defaultMessage: 'Show Github repositories by',
+  },
+  trymeAtPrefix: {
+    id: 'boilerplate.containers.HomePage.tryme.atPrefix',
+    defaultMessage: '@',
   },
 });
